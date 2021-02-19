48cc5ecc23ef4d39869a14eb6ec0f85e9bcfc9c7
Bruno Gabriel Cal Olivera <br1calolivera@gmail.com>
2021-02-19 00:08:46 -0300

    some changes for live server

--- src/components/TrustPilotCarouselItem.jsx
+++ src/components/TrustPilotCarouselItem.jsx
@@ -3,23 +3,7 @@ import React from "react";
 import "../styles/TrustPilotCarousel.css";
 import star from "../static/star-solid.png";
 
-
 const TrustPilotCarouselItem = ({ content }) => {
-  function chunkString(str, len) {
-    const size = Math.ceil(str.length / len);
-    const r = Array(size);
-    let offset = 0;
-
-    for (let i = 0; i < size; i++) {
-      r[i] = str.substr(offset, len);
-      offset += len;
-    }
-
-    return r;
-  }
-
-  const desc = chunkString(content.review_desc, content.review_desc.length / 2);
-
   return (
     <>
       <div className="TPItem__all">
@@ -27,7 +11,7 @@ const TrustPilotCarouselItem = ({ content }) => {
           <blockquote>
             <h1>{content.review_title}</h1>
             <blockquote>
-              <p>{`${desc[0]}...`}</p>
+              <p>{`${content.review_desc}`}</p>
             </blockquote>
           </blockquote>
         </div>
